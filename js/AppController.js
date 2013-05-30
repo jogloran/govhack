944b6dafbec95fda3a815899ce673436b0cad314
Daniel Tse <daniel@semanaut.com>
2013-05-31 00:26:14 +1000

    implemented (but disabled) bootstrap affix support (it's broken)

--- js/AppController.js
+++ js/AppController.js
@@ -1,5 +1,11 @@
 app = angular.module('App', ['ui.bootstrap', 'ngSanitize']);
 
+app.directive('ghAffix', function () {
+    return function (scope, element, attrs) {
+        element.affix({ offset: 67 });
+    };
+});
+
 function AppController($scope) {
 	$scope.world = 'Hello!';
 
